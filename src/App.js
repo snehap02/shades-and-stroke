fb1cd2592eb3d44457fd652b3b1f0290c7c7eac6
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-06-20 15:35:55 +0530

    image section for mobile view

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./index.css";
 function App() {
   return (
     <>
-      <div className="bg-[#080919] w-full h-[200vh] flex flex-col">
+      <div className="bg-[#200b41] w-full h-[200vh] flex flex-col">
         {/* navbar */}
         <div className="p-4 lg:pl-16 fixed">
           <img src="../images/icon.png" className="w-10 lg:w-14" />
@@ -18,15 +18,17 @@ function App() {
         </div>
 
         {/* Hero section */}
-        <div className="grid grid-cols-1 px-10">
+        <div className="grid grid-cols-1 px-8">
           <div className="flex justify-center items-center text-center">
             <h1 className="text-neutral-500 font-robotoMono pt-10">
               A VS Code Dark theme based on one dark pro with shades and strokes
               of beautiful colors for better coding.
             </h1>
           </div>
-          <div className="bg-white mt-20 md:mt-0 h-full">
-            
+          <div className="bg-[#200b41] mt-20 md:mt-0 h-full flex overflow-x-auto gap-10 scrollbar-thin scrollbar-track-[#351962] lg:scrollbar-none">
+            <img src="../images/vs1.jpeg" className=""/>
+            <img src="../images/vs2.jpeg" className=""/>
+            <img src="../images/vs3.jpeg" className=""/>
           </div>
         </div>
       </div>
